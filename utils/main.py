105fda84608f2af4540d52f7974eeca9bda197ce
Diarmid Rendell <diarmid@hotmail.ca>
2024-10-03 16:04:56 -0600

    Created script to convert source XML data to JSON data

--- utils/main.py
+++ utils/main.py
@@ -1,6 +1,28 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+# XML data from https://github.com/cmeeren/zelda-optimal-equipment-guides
+import xml.etree.ElementTree as ET
+import json
+from zelda_data import parse_zelda_data
 
+# Set the XML file path
+xml_file_path = 'xml_source/wind-waker-hd.xml'
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Parse the XML file
+tree = ET.parse(xml_file_path)
+root = tree.getroot()
+
+# Call the parsing function
+game_data = parse_zelda_data(root)
+
+# Convert game_data dictionary to JSON format
+json_data = json.dumps(game_data, indent=4)
+
+# JSON output path
+json_file_path = 'output/game_segments.json'
+
+# Write JSON data to file
+with open(json_file_path, 'w') as json_file:
+    json_file.write(json_data)
+
+print(f"Game data successfully written to {json_file_path}")
+
+print(json_data)
---
+++ utils/zelda_data.py
@@ -0,0 +1,35 @@
+def parse_zelda_data(root):
+    # Create a dictionary to store game segments and their entries
+    game_segments = {}
+
+    # Iterate through each 'gameSegment' element
+    for segment in root.findall('.//gameSegment'):
+        segment_header = segment.find('heading').text.strip() if segment.find('heading') is not None else None
+        segment_id = segment.get('id')
+        entries = []
+
+        # Iterate through each 'entry' within the current game segment
+        for entry in segment.findall('.//entry'):
+            # Strip subheader and check for empty text
+            subheader = entry.text.strip() if entry.text is not None else None
+            if subheader == '':
+                subheader = None # Set out subheader to none if text is empty
+
+            entry_data = {
+                'id': entry.get('id') if entry.get('id') is not None else None,
+                'itemRef': entry.get('itemRef') if entry.get('itemRef') is not None else None,
+                'checkbox': entry.get('checkbox') if entry.get('checkbox') is not None else None,
+                'filters': entry.get('filters') if entry.get('filters') is not None else None,
+                'type': entry.get('type') if entry.get('type') is not None else None,
+                'subheader': subheader,
+                'description': entry.find('description').text if entry.find('description') is not None else None,
+            }
+            entries.append(entry_data)
+
+        # Store the entries in the game_segments dictionary
+        game_segments[segment_id] = {
+            'header': segment_header,
+            'entries': entries
+        }
+
+    return game_segments
